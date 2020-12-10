e45413ac573f8f784b15c2b96a41493c129b4a0c
dayday <530663697@qq.com>
2020-12-10 19:27:30 +0800

    我的项目

---
+++ src/views/category/category.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id = 'category'>
+    <scroll class="category">
+      <ul>
+        <li>aa</li>
+        <button @click="click1">点我！</button>
+        <li @click="click2">aa</li>
+        <div @click="click3">bb</div>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+      </ul>
+    </scroll>
+    <div class="aa">
+      11
+    </div>
+    <div class="bb">22</div>
+  </div>
+</template>
+
+<script>
+  // import BScroll from 'better-scroll';
+  import Scroll from 'components/common/scroll/Scroll';
+  export default {
+    name: "category",
+    data(){
+      return {
+        bscroll:null
+      }
+    },
+    components:{
+      Scroll
+    },
+    methods:{
+      click1(){
+        console.log('1呗点了');
+      },
+      click2(){
+        console.log('2被点了');
+      },
+      click3(){
+        console.log('3呗点了');
+      }
+
+    }
+
+    // mounted(){
+    //   this.bscroll = new BScroll('#category',{
+    //     probeType:2,
+    //     pullUpLoad:true
+    //   })
+    //   this.bscroll.on('scroll',(position)=>{
+    //     // console.log(position);
+    //   });
+    //   this.bscroll.on('pullingUp',()=>{
+    //     console.log('Upload');
+    //     this.bscroll.finishPullUp();
+    //   })
+    // }
+  }
+
+</script>
+
+<style scoped>
+  .category{
+    background-color: red;
+    height: 150px;
+    overflow: hidden;
+    /*overflow-y: scroll;*/
+  }
+  li{
+    font-size:20px;
+  }
+  .aa{
+    width: 100px;
+    height: 100px;
+    background-color: pink;
+    float: left;
+  }
+  .bb{
+    background-color: yellow;
+    width: 150px;
+    /*height: 100px;*/
+  }
+</style>
---
+++ src/views/shopcar/childComponents/ShopCarList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="shop-car-list">
+      <scroll class="content" ref="scroll">
+        <shop-car-item v-for="item in product"
+                       :product="item"
+                       class="shop-car-item"/>
+      </scroll>
+    </div>
+</template>
+
+<script>
+  import Scroll from 'components/common/scroll/Scroll';
+  import {mapGetters} from 'vuex';
+  import ShopCarItem from './ShopCarItem';
+    export default {
+      name: "shopCarList",
+      components:{
+        Scroll,
+        ShopCarItem
+      },
+      computed:{
+        ...mapGetters({
+          product:'getCartProduct'
+        })
+      },
+      activated(){
+        this.$refs.scroll.refresh();
+      }
+    }
+</script>
+
+<style scoped>
+  .shop-car-list{
+    height: calc(100% - 44px - 49px - 44px);
+  }
+  .content{
+    height: 100%;
+    /*height: 100%;*/
+    overflow: hidden;
+  }
+</style>
